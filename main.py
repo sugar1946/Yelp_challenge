30c3f94742d07a5cfa29f311dda0c442c5a04b03
Chaofei Yang <ycf418@gmail.com>
2016-03-20 16:58:31 -0400

    restaurant reviews with votes more than one

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import json
 business_addr = 'yelp_academic_dataset_business.json'
 review_addr = 'yelp_academic_dataset_review.json'
 business_list = [];
+
 with open(business_addr) as input_file:
     for line in input_file:
         line_content = json.loads(line)
         if 'Restaurants' in line_content['categories']:
             business_list.append(line_content['business_id'])
+input_file.close()
 
-
-
-'''
-obj = {'a':12,'b':23}
-print obj
-print type(obj)
-print obj['a']
-encodedjson = json.dumps(obj)
-print encodedjson
-print type(encodedjson)
-print encodedjson[2]
-decodedjson = json.loads(encodedjson)
-print decodedjson
-print type(decodedjson)
-print decodedjson['a']
-'''
+output_file = open('restaurant_review.json','w')
+with open(review_addr) as input_file:
+    for line in input_file:
+        line_content = json.loads(line)
+        vote = line_content['votes']['funny']+line_content['votes']['useful']+line_content['votes']['cool']
+        if vote > 0 and line_content['business_id'] in business_list:
+            output_content = json.dumps(line_content)
+            output_file.write(output_content + '\n')
+input_file.close()
+output_file.close()
